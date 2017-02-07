e04579dcd343a47dab9c88b3866f3c05cf415262
axgtz <axguti@gmail.com>
2017-02-06 20:12:57 -0600

    Update main and array
    added lectura de datos, although the functions are not recognising the array of the class

--- Busqueda/Arreglo.h
+++ Busqueda/Arreglo.h
@@ -1,36 +1,61 @@
 //Roberto Alejandro Gutiérrez Guillén A01019608
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-class Arreglo(){
+class Arreglo{
 private:
-  //int a[];
+    int arreglo[];
 public:
-	 Arreglo();
-  // Lee los datos de una archivo cuyo nombre recibe como parámetro
-  void lecturaDatos(string archivo){
+	Arreglo();
 
+  void lecturaDatos(string archivo){  // Lee los datos de una archivo cuyo nombre recibe como parámetro
+	  ifstream archivo_entrada;
+	  char linea[128];
+
+	  string s = archivo + ".txt";
+
+	  archivo_entrada.open(s);
+
+	  if (archivo_entrada.fail()) {
+		  cout << "Error al abrir el archivo" << endl;
+		  arreglo[0] = { -1 };
+	  }
+
+	  while (!archivo_entrada.eof()) {
+		  archivo_entrada.getline(linea, sizeof(linea));
+
+		  string str(linea);
+
+		  istringstream iss(str);
+
+		  string sub;
+		  iss >> sub;
+		  arreglo.push_back(sub);
+
+	  }
+	  archivo_entrada.close();
   }
 
   // busca el elemento K en el arreglo, usando el algoritmo SECUENCIAL. Si lo  
-  // encuentra devuelve su posición y en caso contrario regresa un ‐1.
-  int busquedaSecuencial(int K){
+  int busquedaSecuencial(int K){  // encuentra devuelve su posición y en caso contrario regresa un ‐1.
     return 1;
   }
 
 
   // busca el elemento K en el arreglo, usando el algoritmo BINARIO. Si lo  
-  // encuentra devuelve su posición y en caso contrario regresa un ‐1.
-  int busquedaBinaria(int inicio, int fin, int K){
+  int busquedaBinaria(int inicio, int fin, int K){  // encuentra devuelve su posición y en caso contrario regresa un ‐1.
     int pos;
     int ini = 0;
-    int fin = n-1;
+    int fin = arreglo.size() -1;
     int mid = (ini+fin)/2;
 
-     while ( ini <= fin && a[mid] != K ){
-         fin = mid-1;
+     while ( ini <= fin && arreglo[mid] != K ){
+		if (arreglo[mid] < K)
+		 fin = mid-1;
        else
          ini = mid+1;
        mid = (ini+fin)/2;
@@ -41,7 +66,7 @@ public:
        pos = mid;
      return pos;
   }
-}
+};
 /*
 public static<T extends Comparable<T>> int busquedaSecOrdenada(T[] a, int n, T x){
   int i=0;
