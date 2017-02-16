b37e7cbb2333eb943c77a998aa8efa3734ef20c2
axgtz <axguti@gmail.com>
2017-02-16 17:12:51 -0600

    Acomodo estructura e insertion sort sirve
    Se cambio un array de pointers por un vector, se separo la declaración
    de métodos y la funcionalidad. Sirve swap,insertion sort. Falta lectura
    de datos

--- Algoritmos-de-Ordenamiento/Algoritmos-de-Ordenamiento/main.cpp
+++ Algoritmos-de-Ordenamiento/Algoritmos-de-Ordenamiento/main.cpp
@@ -7,57 +7,101 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-template <class T>
-class manejadorArreglosGenerico {
-    int tam = 0;
-    T *arr;
+template <class Type>
+
+class ManejadorArreglosGenerico {
+    
+private:
+    int tam;
+    vector <Type> vec;
     
 public:
-    manejadorArreglosGenerico(int tam,T arr[]){
-        this->tam = tam;
-        this->arr = arr[tam];
-    }
+    ManejadorArreglosGenerico(string nombreArchivo,  vector <Type> v, int tam);
     
-    void imprimeArreglo(){
-        for(int i = 0 ; i < tam; i++){
-            cout << arr[i] << " - ";
-        }
-        cout << endl;
-    }
+    void lecturaDatos(string nombreArchivo);
     
+    void swap(int a,int b);
     
-    void swap(T a,T b,T arr[]){
-        T temporal = arr[b];
-        arr[b] = arr[a];
-        arr[a] = temporal;
-    }
+    void print();
     
+    //Metodos de Ordenamiento
+    void selectionSort();
     
-    void selectionSort(T arr[],int num){
-        int posMenor;
-        
-        for(int i = 1;i>num;i++){
-            posMenor = i;
-            for(int j = i+1 ; i>num ; j++){
-                if(arr[posMenor] > arr[j]){//Se encuentra uno menor que el num seleccionado
-                    posMenor = j;
-                }
-            }
-            swap(arr[i],arr[posMenor],arr);
-        }
-        imprimeArreglo();
-    }
-    
+    void insertionSort();
 };
+
+template <class Type>   //Se tiene que poner antes de cada metodo
+ManejadorArreglosGenerico<Type>::ManejadorArreglosGenerico(string nombreArchivo,  vector <Type> v, int tam) {
+    this->tam = tam;
+    vec = v;
+}
+
+template <class Type>
+void ManejadorArreglosGenerico<Type>::swap(int a,int b){
+    Type temporal = vec[b];
+    vec[b] = vec[a];
+    vec[a] = temporal;
+}
+
+template <class Type>
+void ManejadorArreglosGenerico<Type>::print(){
+    for(int i = 0 ; i < tam; i++){
+        cout << vec[i] << " - ";
+    }
+    cout << endl;
+}
+
+template <class Type>
+void ManejadorArreglosGenerico<Type>::selectionSort(){
+    int posMenor;
+    print();
+    for(int i = 0; i < tam;i++){
+        posMenor = i;
+        for(int j = i + 1 ; j < tam ; j++){
+            if(vec[posMenor] > vec[j]){//Se encuentra uno menor que el num seleccionado
+                posMenor = j;
+            }
+        }
+        //swap(vec[i],vec[posMenor],arr);
+        swap(i,posMenor);
+        print();//Imprimir despues del swap
+    }
+    
+}
+
+
+template <class Type>
+void ManejadorArreglosGenerico<Type>::insertionSort() {
+    for(int i =0 ; i < tam; i++){
+        
+        
+    }
+}
+
+
+/*
+ 
+ 
+    void bubbleSort(){
+        bool bandera = false;
+        
+        //if swap, si bandera es false cuando acabe, entonces se acaba el while
+        bandera = true;
+    }
+    
+
+ */
 int main(int argc, const char * argv[]) {
+     vector <int> v = {90,70,00,50,30,10,60,80,20,40};
     
-    
-    
-    
-    
+    ManejadorArreglosGenerico<int> *a = new  ManejadorArreglosGenerico<int>("pepe",v, 10);
+    a->print();
+    cout << "Selection Sort: " << endl;
+    a->selectionSort();
     
     return 0;
 }
