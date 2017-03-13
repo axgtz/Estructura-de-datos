578448ab7c96d7a3da90d5eb71c80eca0fad3590
axgtz <axguti@gmail.com>
2017-03-13 13:03:08 -0600

    Almost finished Pila stun ADT
    Faltan un par de tests

--- Pila/Pila/Pila.cpp
+++ Pila/Pila/Pila.cpp
@@ -1,61 +1,95 @@
-//Roberto Alejandro Gutiérrez Guillén			A01019608
+//Roberto Alejandro Gutierrez Guillen			A01019608
 #include <iostream>
 
 using namespace std;
 
 class Pila {
 private:
-	int* arr;
-	int MAX;
+	int *pila;
+	int top;
+    int MAX;
 
 public:
 	Pila(int max);
 
-	// Inserta un elemento en la pila. Regresa true si la operación  
+	// Inserta un elemento en la pila. Regresa true si la operacion
 	// fue exitosa y false en caso contrario.
 	bool push(int dato);
-	// Saca un elemento de la pila. Regresa true si la operación fue  
+	// Saca un elemento de la pila. Regresa true si la operacion fue
 	// exitosa y false en caso contrario.
 	bool pop(int &dato);
-	// Regresa true si la pila está llena.
+	// Regresa true si la pila esta llena.
 	bool pilaLlena();
-	// Regresa true si la pila está vacía.
+	// Regresa true si la pila esta vacia.
 	bool pilaVacia();
-	// Imprime el valor de la variable top y todos los elementos de la  
+	// Imprime el valor de la variable top y todos los elementos de la
 	// pila en una pila en una fila.
 	void imprimePila();
 };
 
 Pila::Pila(int max){
-	MAX = max;
-	arr[max];
+    if(max < 0) cout << "No se puede tener un tamano menor a 1" << endl;
+	this->MAX = max;
+	pila = new int[MAX];       //Asignar el espacio de la pila
+    top = -1;
 }
 
 
 bool Pila::push(int dato) {
+    if(pilaLlena())return false;
+    top++;
+    pila[top] = dato;
+    
 	return true;
 }
 
 
 bool Pila::pop(int &dato) {
-	return true;
+    if(pilaVacia())return false;
+    dato = pila[top];      //El valor que se encuentra dentro de la pila no se reescribe porque solo
+    top--;                  //se gastaria tiempo,  se mueve el top entonces no se puede acceder al valor
+    return true;            //y cuando se agrege un elemento es cuando se sobreescribe
 }
 
 
 bool Pila::pilaLlena() {
-	return true;
+    if(top == MAX-1)return true;
+    return false;
 }
 
 
 bool Pila::pilaVacia() {
-	return true;
+    if(top == -1)return true;//Esta vacia
+    return false;
 }
 
 
 void Pila::imprimePila() {
-
+    cout << pila[top] << "<- tope" << endl;
+    for (int i = top; i >= 0; i--) {
+        cout << pila[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
-	
+    Pila p = Pila(5);
+    
+    p.push(2);
+    p.push(5);
+    
+    p.imprimePila();
+    
+    int h;
+    p.pop(h);
+    
+    cout << h << endl;
 }
+
+
+
+
+
+
+
+
