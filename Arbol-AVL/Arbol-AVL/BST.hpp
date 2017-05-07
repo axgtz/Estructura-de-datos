308c73e6ab69d1ad3f423d96bb0ab9c11eb4146f
axgtz <axguti@gmail.com>
2017-05-07 15:04:07 -0500

    change name

---
+++ Arbol-AVL/Arbol-AVL/BST.hpp
@@ -0,0 +1,24 @@
+//Roberto Alejandro Gutiérrez Guillén		A01019608
+#include <iostream>
+
+//Librerias de lectura de datos
+#include <fstream>
+#include <sstream>
+
+#ifndef BST_HPP
+#define BST_HPP
+
+using namespace std;
+
+struct Node {
+	int data = NULL;
+	Node *pointIzq = NULL;
+	Node *pointDer = NULL;
+};
+
+class Arbol {
+private:
+
+public:
+};
+#endif
