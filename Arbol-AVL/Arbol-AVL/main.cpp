ec9f6c738e89f3fd261197ba91eea363a49f61b7
axgtz <axguti@gmail.com>
2017-09-13 01:41:16 -0500

    Avl doesn't replace root correctly

--- Arbol-AVL/Arbol-AVL/main.cpp
+++ Arbol-AVL/Arbol-AVL/main.cpp
@@ -1,6 +1,6 @@
 //  main.cpp
 //  Created by Alex Gutz on 5/06/17.
-//  Copyright © 2017 Alejandro Gutierrez. All rights reserved.
+//  Copyright ï¿½ 2017 Alejandro Gutierrez. All rights reserved.
 
 #include <iostream>
 #include "BST.h"
@@ -18,68 +18,68 @@ int main(int argc, const char * argv[]) {
 		cout << "\nSe encontro: " << cN->data;
 	}
 
+    a.elimina(15);
 	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(15);
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(10);
+    a.elimina(10);
+	cout << "\n\n Eliminar: 10\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(20);
+    a.elimina(20);
+	cout << "\n\n Eliminar: 20\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(30);
+    a.elimina(30);
+	cout << "\n\n Eliminar: 30\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(50);
+    a.elimina(50);
+	cout << "\n\n Eliminar: 50\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(75);
+    a.elimina(75);
+	cout << "\n\n Eliminar: 75\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(80);
+    a.elimina(80);
+	cout << "\n\n Eliminar: 80\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(90);
+    a.elimina(90);
+	cout << "\n\n Eliminar: 90\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(70);
+    a.elimina(70);
+	cout << "\n\n Eliminar: 70\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(76);
+    a.elimina(76);
+	cout << "\n\n Eliminar: 76\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(60);
+    a.elimina(60);
+	cout << "\n\n Eliminar: 60\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(100);
+    a.elimina(100);
+	cout << "\n\n Eliminar: 100\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(25);
+    a.elimina(25);
+	cout << "\n\n Eliminar: 25\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(40);
+    a.elimina(40);
+	cout << "\n\n Eliminar: 40\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(0);
+    a.elimina(0);
+	cout << "\n\n Eliminar: 0\t Raiz:" << a.raiz->data;
 	a.imprime();
 
-	cout << "\n\n Eliminar: 15\t Raiz:" << a.raiz->data;
-	a.elimina(85);
+    a.elimina(85);
+	cout << "\n\n Eliminar: 85\t Raiz:" << a.raiz->data;
 	a.imprime();
 
 	int x;
